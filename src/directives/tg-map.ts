81d850583cbdefc8ed0c473ec0ba2f87c28bb09c
Steven Lei <stevenplus@gmail.com>
2021-10-31 16:16:28 +0800

    use … instead of - to handle negative numbers; handle increments

--- src/directives/tg-map.ts
+++ src/directives/tg-map.ts
@@ -1,4 +1,6 @@
-export function get(value?: string) {
+import { TgElementExtraData } from '../type';
+
+export function get(value?: string, data?: TgElementExtraData) {
   const items: Record<string, string> = {};
 
   if (typeof value === 'string' && value.trim() !== '') {
@@ -11,13 +13,22 @@ export function get(value?: string) {
           arr[0].split(',').forEach((key) => {
             items[key.trim()] = arr[1].trim();
           });
-        }else if(arr[0].indexOf('-') > -1){
-          let [from, to] = arr[0].split('-').map((val)=>{
-            return +val
-          }).sort((a, b) => a - b)
+        } else if (arr[0].indexOf('...') > -1) {
+          // Use `...` here to define a range, inspired by Swift range operator.
+          // Instead of using `-`, we can handle negative numbers easily.
+          let [from, to] = arr[0]
+            .split('...')
+            .map((val) => {
+              return +val;
+            })
+            .sort((a, b) => a - b);
 
-          for(let i = from; i <= to; i++){
+          let i = from;
+          while (i <= to) {
+            i = Number(i.toFixed(data?.decimals || 2));
             items[i] = arr[1].trim();
+
+            i += data?.increment || 0.01;
           }
         } else {
           items[arr[0].trim()] = arr[1].trim();
@@ -25,7 +36,7 @@ export function get(value?: string) {
       }
     });
   }
-console.log(items);
+  console.log(items);
 
   return items;
 }
