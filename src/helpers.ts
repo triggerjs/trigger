8555f4bc0234653562b7667213d44c78104c394b
Arman.H <ahhdev@gmail.com>
2021-10-31 10:34:37 +0330

    refactor: there is no need to call valueOf explicitly

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,6 +4,6 @@
  * @returns {number} The caculated length.
  */
 export function decimalsLength(number: number): number {
-  if (Math.floor(number.valueOf()) === number.valueOf()) return 0;
+  if (Math.floor(number) === number) return 0;
   return number.toString().split('.')[1].length || 0;
 }
