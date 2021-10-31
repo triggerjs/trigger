e416282f32bc5cbc2f247a38fe07afcccede2a84
plainheart <yhen@all-my-life.cn>
2021-10-31 19:19:29 +0800

    fix: 1) rename `tg-top` & `tg-height` variables if the prefix is customized. 2) avoid unnecessary calls on `getPrefix` function. 3) simplify `getPrefixSetting` function & `setPrefix` function. 4) remove unnecessary typescript `as`. (triggerjs/trigger#18) 5) tweak some comments and correct some typos.

--- src/directives/tg-bezier.ts
+++ src/directives/tg-bezier.ts
@@ -1,19 +1,19 @@
 import { defaultBezier } from '../ease';
 
-export function get(value?: String) {
+export function get(value?: string) {
   if (typeof value === 'string') {
     if (value.indexOf(',') > -1) {
-      let arr = value.split(',');
+      const arr = value.split(',');
       if (arr.length !== 4) {
         throw new Error(
           `Bezier function expected 4 arguments, but got ${arr.length}.`
         );
       }
       return arr;
-    } else if (!defaultBezier.hasOwnProperty(value as string)) {
+    } else if (!defaultBezier.hasOwnProperty(value)) {
       // Available named bezier values: `ease`, `easeIn`, `easeOut`, `easeInOut`
       throw new Error(
-        'The default value of the bazier function does not exist！'
+        'The default value of the bezier function does not exist！'
       );
     }
     return value;
--- src/prefix.ts
+++ src/prefix.ts
@@ -3,23 +3,15 @@ import { Prefix } from './type';
 let prefix = `tg`;
 
 export function getPrefixSetting() {
-  if (
-    typeof document.body === 'undefined' ||
-    !document.body.hasAttribute('data-trigger-prefix')
-  ) {
-    return;
-  }
-
-  const newPrefix = document.body.getAttribute('data-trigger-prefix');
+  const newPrefix = document.body && document.body.getAttribute('data-trigger-prefix');
   newPrefix && setPrefix(newPrefix);
 }
 
 function setPrefix(str: string) {
-  if (typeof str !== 'string' || str.trim() === '') {
+  if (typeof str !== 'string' || !(str = str.trim())) {
     return;
   }
 
-  str = str.trim();
   prefix = str;
 }
 
